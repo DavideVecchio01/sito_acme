a99f32cc69525af48d64c07f9dacb147d81b13a2
DavideVecchio01 <95300391+DavideVecchio01@users.noreply.github.com>
2022-06-21 10:54:20 +0200

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function scrollToTop() {
+           window.scrollTo(0, 0);
+        }
+function showCookie() {
+      let cookie = document.querySelector('#cookie');
+      cookie.style = "display:block;";
+    }
+//cookie
+function hideCookie() {
+      let cookie = document.querySelector('#cookie');
+      cookie.style = "display:none;";
+    }
